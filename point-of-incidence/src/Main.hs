ee4ddbc90387ce94cde878705124b92ddf7531dc
Rene Saarsoo <nene@triin.net>
2023-12-13 10:21:56 +0200

    Day 13: Parse input

--- point-of-incidence/src/Main.hs
+++ point-of-incidence/src/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
+import Data.List.Utils (split)
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  putStrLn input
+  print $ parseInput input
+
+type Puzzle = [String]
+
+parseInput :: String -> [Puzzle]
+parseInput input = lines <$> split "\n\n" input
