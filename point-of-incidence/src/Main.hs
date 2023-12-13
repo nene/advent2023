c7f4087be61949b56f19a2831ad20d6dabe78a28
Rene Saarsoo <nene@triin.net>
2023-12-13 12:58:51 +0200

    Day 13: Unfinished part 2

--- point-of-incidence/src/Main.hs
+++ point-of-incidence/src/Main.hs
@@ -1,23 +1,51 @@
 module Main (main) where
 
-import Data.List.Utils (split)
-import Data.List (transpose)
+import Data.List.Utils (split, uniq)
+import Data.List (transpose, find)
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ sum $ puzzleScore <$> parseInput input
+  let puzzles = parseInput input
+  print $ newPuzzleScore2 <$> (take 10 $ drop 20 puzzles)
 
 type Puzzle = [String]
 
 parseInput :: String -> [Puzzle]
 parseInput input = lines <$> split "\n\n" input
 
-puzzleScore :: Puzzle -> Int
-puzzleScore xs = rowsScore*100 + colsScore
+newPuzzleScore :: Puzzle -> Int
+newPuzzleScore pz = snd $ newPuzzleScore2 pz
+
+newPuzzleScore2 :: Puzzle -> ((Int, [Int], Int, [Int]), Int)
+newPuzzleScore2 pz = (input, result)
   where
-    rowsScore = reflectionScore $ reflection xs
-    colsScore = reflectionScore $ reflection $ transpose xs
+    result = case input of
+      (_, [], _, []) -> 0
+      (_, [r], _, []) -> r
+      (r', rs, _, []) -> head $ filter (/= r') rs
+      (_, [], _, [c]) -> c
+      (_, [], c', cs) -> head $ filter (/= c') cs
+      (r', [r], c', [c]) -> if r == r' && c /= 0 then c else r
+      (r', rs, c', cs) -> case (filter (/=r') rs, filter (/=c') cs) of
+        ((r:rs), _) -> r
+        (_, (c:cs)) -> c
+    input = (oldRow * 100, (*100) <$> newRows, oldCol, newCols)
+    newRows = uniq $ filter (>0) $ rowScore <$> smudge pz
+    newCols = uniq $ filter (>0) $ colScore <$> smudge pz
+    oldRow = rowScore pz
+    oldCol = colScore pz
+
+oldPuzzleScore :: Puzzle -> Int
+oldPuzzleScore pz = rowScore pz * 100 + colScore pz
+
+rowScore :: Puzzle -> Int
+rowScore = reflectionScore . reflection
+
+colScore :: Puzzle -> Int
+colScore = reflectionScore . reflection . transpose
 
 reflectionScore :: Maybe (Puzzle, Puzzle) -> Int
 reflectionScore (Just (xs, _)) = length xs
@@ -36,3 +64,16 @@ possibleReflections xs = map (`reflectionAt` xs) [1..(length xs - 1)]
 
 reflectionAt :: Int -> Puzzle -> (Puzzle, Puzzle)
 reflectionAt n xs = (reverse $ take n xs, drop n xs)
+
+smudge :: Puzzle -> [Puzzle]
+smudge pz = [chunksOf lineLen (invertAt n linePz) | n <- [0..(length linePz - 1)]]
+  where
+    lineLen = length (head pz)
+    linePz = concat pz
+
+invertAt :: Int -> String -> String
+invertAt _ "" = ""
+invertAt 0 ('.':xs) = '#' : xs
+invertAt 0 ('#':xs) = '.' : xs
+invertAt n (x:xs) = x : invertAt (n-1) xs
+
