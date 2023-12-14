f6de0eb613fb88ff50fe9227a54f28ace0e727e9
Rene Saarsoo <nene@triin.net>
2023-12-14 11:39:29 +0200

    Day 14: Part 1 solved

--- parabolic-reflector-dish/src/Main.hs
+++ parabolic-reflector-dish/src/Main.hs
@@ -1,7 +1,28 @@
 module Main (main) where
+import Data.List (transpose)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ lines input
+  print $ score $ tiltNorth $ lines input
 
+tiltNorth :: [String] -> [String]
+tiltNorth xs = turnLeft $ tiltLine <$> turnRight xs
+  where
+    turnLeft = reverse . transpose
+    turnRight = transpose . reverse
+
+tiltLine :: String -> String
+tiltLine = tiltLine' []
+  where
+    tiltLine' carry "" = carry
+    tiltLine' carry ('O':xs) = tiltLine' ('O':carry) xs
+    tiltLine' carry ('.':xs) = '.' : tiltLine' carry xs
+    tiltLine' carry ('#':xs) = carry ++ "#" ++ tiltLine' [] xs
+    tiltLine' _ (c:_) = error ("Unexpected symbol: " ++ [c])
+
+score :: [String] -> Int
+score xs = sum $ zipWith scoreLine [1..] (reverse xs)
+
+scoreLine :: Int -> String -> Int
+scoreLine v xs = length (filter (=='O') xs) * v
