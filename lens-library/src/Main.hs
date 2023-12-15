4092a83b8a238eea6a5b756facc3ee871489fa6a
Rene Saarsoo <nene@triin.net>
2023-12-15 10:19:29 +0200

    Day 15: Part 1 done

--- lens-library/src/Main.hs
+++ lens-library/src/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
+import Data.Char (ord)
+import Data.List.Utils (split)
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print input
+  print $ sum $ hash <$> split "," input
+
+hash :: String -> Int
+hash = foldl op 0
+  where
+    op acc c = (acc + ord c) * 17 `mod` 256
