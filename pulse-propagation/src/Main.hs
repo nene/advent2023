f5eba6abd02e1e7887cdb97edce173b72a199f7a
Rene Saarsoo <nene@triin.net>
2023-12-20 14:15:32 +0200

    Day 20: Process single pulse

--- pulse-propagation/src/Main.hs
+++ pulse-propagation/src/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 import Data.List.Utils (split)
 import Data.Foldable (find)
-import Data.Map.Strict (Map)
+import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
 
 main :: IO ()
@@ -11,12 +11,15 @@ main = do
   putStrLn $ "Broadcast to: " ++ show broadcastDests
   putStrLn ""
   print moduleMap
+  print $ process (moduleMap ! "a") ("broadcast", Low, "a")
+
+type ModuleMap = Map String Module
 
 data Module = FlipFlop State [String] | Conjunction [(String, Pulse)] [String] deriving (Show)
 
-data State = On | Off deriving (Show)
+data State = On | Off deriving (Show, Eq)
 
-data Pulse = Low | High deriving (Show)
+data Pulse = Low | High deriving (Show, Eq)
 
 type RawModule = (Char, String, [String])
 
@@ -39,7 +42,7 @@ extractBroadcastDestinations modules = case find isBroadcaster modules of
   where
     isBroadcaster (_, name, _) = name == "broadcaster"
 
-buildModuleMap :: [RawModule] -> Map String Module
+buildModuleMap :: [RawModule] -> ModuleMap
 buildModuleMap rawModules = Map.fromList $ nameModulePair <$> rawModules
   where
     nameModulePair ('%', name, ds) = (name, FlipFlop Off ds)
@@ -50,3 +53,25 @@ buildModuleMap rawModules = Map.fromList $ nameModulePair <$> rawModules
     inputsOf name = extractName <$> filter (\(_, _, ds) -> name `elem` ds) rawModules
 
     extractName (_, name, _) = name
+
+
+
+-- sendPulses :: [(String, Pulse, String)] -> ModuleMap -> [Pulse]
+-- sendPulses ((from, p, to):pulses) moduleMap = process (moduleMap ! to) (from, p, to)
+
+
+process :: Module -> (String, Pulse, String) -> (Module, [(String, Pulse, String)])
+-- If a flip-flop module receives a high pulse, it is ignored and nothing happens.
+process m@(FlipFlop _ _) (_, High, _) = (m, [])
+-- if a flip-flop module receives a low pulse, it flips between on and off.
+-- If it was off, it turns on and sends a high pulse.
+-- If it was on, it turns off and sends a low pulse.
+process (FlipFlop Off ds) (_, Low, to) = (FlipFlop On ds, [(to, High, d) | d <- ds])
+process (FlipFlop On ds) (_, Low, to) = (FlipFlop Off ds, [(to, Low, d) | d <- ds])
+-- When a pulse is received, the conjunction module first updates its memory for that input
+-- Then, if it remembers high pulses for all inputs, it sends a low pulse;
+-- otherwise, it sends a high pulse.
+process (Conjunction inputs ds) (from, inPulse, to) = (Conjunction updatedInputs ds, [(to, outPulse, d) | d <- ds])
+  where
+    updatedInputs = (\(name, p) -> (name, if name == from then inPulse else p)) <$> inputs
+    outPulse = if all ((==High) . snd) inputs then Low else High
