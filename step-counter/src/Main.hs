f4a0266578b9209e41d4910011a7164dd392c1d2
Rene Saarsoo <nene@triin.net>
2023-12-21 10:17:20 +0200

    Day 21: Parse input

--- step-counter/src/Main.hs
+++ step-counter/src/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
+import Vector2D (Vector2D, Coord2D)
+import qualified Vector2D
+import Data.Maybe (fromMaybe)
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print input
+  let (gardenPlan, startCoord) = parseInput input
+  print startCoord
+  print gardenPlan
+
+parseInput :: String -> (Vector2D Char, Coord2D)
+parseInput input = (cleanVector, startCoord)
+  where
+    cleanVector = Vector2D.map (\v -> if v == 'S' then '.' else v) rawVector
+    startCoord = fromMaybe (-1, -1) $ Vector2D.findCoord (=='S') rawVector
+    rawVector = Vector2D.fromList $ lines input
---
+++ step-counter/src/Vector2D.hs
@@ -0,0 +1,57 @@
+module Vector2D (Vector2D, Coord2D, fromList, toList, at, setAt, updateAt, size, Vector2D.map, fill, findCoord) where
+
+import Data.Vector (Vector, (!?), (//))
+import qualified Data.Vector as Vector
+import Data.Maybe (isJust)
+
+type Vector2D a = Vector (Vector a)
+
+type Coord2D = (Int, Int)
+
+-- 2D list to Vector2D
+fromList :: [[a]] -> Vector2D a
+fromList xs = Vector.fromList (Vector.fromList <$> xs)
+
+-- Vector2D to 2D list
+toList :: Vector2D a -> [[a]]
+toList vector = Vector.toList <$> Vector.toList vector
+
+-- value at coordinate
+at :: Vector2D a -> Coord2D -> Maybe a
+at rows (rowIndex, colIndex) = do
+  row <- rows !? rowIndex
+  row !? colIndex
+
+-- Set value at coordinate
+setAt :: Coord2D -> a -> Vector2D a -> Vector2D a
+setAt (rowIndex, colIndex) value rows = rows // [(rowIndex, newRow)]
+  where
+    newRow = case rows !? rowIndex of
+      Nothing -> Vector.empty
+      Just row -> row // [(colIndex, value)]
+
+-- Update value at coordinate
+updateAt :: Coord2D -> (a -> a) -> Vector2D a -> Vector2D a
+updateAt coord fn vector = case vector `at` coord of
+  Nothing -> vector
+  Just oldValue -> setAt coord (fn oldValue) vector
+
+-- (height, width) of a 2D vector
+size :: Vector2D a -> Coord2D
+size rows = (length rows, maybe 0 length (rows !? 0))
+
+-- a map over 2D vector
+map :: (a -> b) -> Vector2D a -> Vector2D b
+map f = Vector.map (Vector.map f)
+
+-- searches for value from 2D vector and returns a coordinate
+findCoord :: (a -> Bool) -> Vector2D a -> Maybe Coord2D
+findCoord fn vect = do
+  rowIndex <- Vector.findIndex (isJust . Vector.findIndex fn) vect
+  row <- vect !? rowIndex
+  colIndex <- Vector.findIndex fn row
+  return (rowIndex, colIndex)
+
+-- creates 2D vector filled with some value
+fill :: Coord2D -> a -> Vector2D a
+fill (rowCount, colCount) v = Vector.replicate rowCount $ Vector.replicate colCount v
