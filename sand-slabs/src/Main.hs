423d6730875ed88ea8c3f5eaee67a0e07c63c237
Rene Saarsoo <nene@triin.net>
2023-12-22 11:03:54 +0200

    Day 22: Parse input

--- sand-slabs/src/Main.hs
+++ sand-slabs/src/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
+import Data.List.Utils (split)
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print input
+  print $ parseInput input
+
+type Coord3D = (Int, Int, Int)
+
+parseInput :: String -> [(Coord3D, Coord3D)]
+parseInput input = parseLine <$> lines input
+  where
+    parseLine line = case split "~" line of
+      [left, right] -> (parseCoord left, parseCoord right)
+      _ -> error "Invalid line"
+
+    parseCoord txt = case split "," txt of
+      [x,y,z] -> (read x, read y, read z)
+      _ -> error "Invalid coordinate"
