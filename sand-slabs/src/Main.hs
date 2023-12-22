c95ead1db50759cdcb68e7c658561d61de7cc838
Rene Saarsoo <nene@triin.net>
2023-12-22 20:52:42 +0200

    Day 22: Switch to solid bricks as main data type

--- sand-slabs/src/Main.hs
+++ sand-slabs/src/Main.hs
@@ -10,13 +10,17 @@ import Data.List (transpose, find)
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  let bricks = parseInput input
-  let size = inputSize bricks
+  let rawBricks = parseInput input
+  let bricks = rawToSolidBrick <$> rawBricks
+  let size = inputSize rawBricks
   let cube = initialCube size
   putStrLn $ visualizeCube $ addAllBricks bricks cube
   putStrLn $ visualizeCube $ fallAllBricks bricks cube
 
-parseInput :: String -> [(Coord3D, Coord3D)]
+type RawBrick = (Coord3D, Coord3D)
+type Brick = [Coord3D]
+
+parseInput :: String -> [RawBrick]
 parseInput input = parseLine <$> lines input
   where
     parseLine line = case split "~" line of
@@ -43,8 +47,7 @@ visualizeCube vect = unlines $ foldl1 sideWays $ planeToStrings <$> planes
     planes :: [[[Int]]]
     planes = reverse <$> transpose (Vector3D.toList vect)
 
-
-inputSize :: [(Coord3D, Coord3D)] -> Coord3D
+inputSize :: [RawBrick] -> Coord3D
 inputSize pairs = (x+1, y+1, z+1)
   where (x, y, z) = maxCoord3D $ concatMap (\(a,b) -> [a, b]) pairs
 
@@ -58,34 +61,30 @@ initialCube (x,y,z) = emptyCube // [(0, filledFloor)]
     emptyCube = Vector3D.fill (x,y,z) 0
     filledFloor = Vector2D.fill (y,z) (-1)
 
-fallAllBricks :: [(Coord3D, Coord3D)] -> Vector3D Int -> Vector3D Int
+fallAllBricks :: [Brick] -> Vector3D Int -> Vector3D Int
 fallAllBricks bricks cube = foldl (\cub (b, i) -> fallBrick b i cub) cube $ zip bricks [1..]
 
-fallBrick :: (Coord3D, Coord3D) -> Int -> Vector3D Int -> Vector3D Int
-fallBrick brick value cube = fillCoords newBrickCoords value cube
+fallBrick :: Brick -> Int -> Vector3D Int -> Vector3D Int
+fallBrick brick value cube = fillCoords newBrick value cube
   where
-    newBrickCoords = translateX (minNewX - minOldX) coords
+    newBrick = translateX (minNewX - minOldX) brick
     translateX dx = map (\(x,y,z) -> (x+dx,y,z))
-    minNewX = maximum $ (`fallCoord` cube) <$> coords
-    minOldX = minimum $ Vector3D.coord3DX <$> coords
-    coords = brickCoords brick
+    minNewX = maximum $ (`fallCoord` cube) <$> brick
+    minOldX = minimum $ Vector3D.coord3DX <$> brick
 
 fallCoord :: Coord3D -> Vector3D Int -> Int
 fallCoord (x,y,z) cube = case find (\ x' -> cube `at` (x', y, z) /= Just 0) (reverse [0 .. x]) of
   Just newX -> newX + 1
   Nothing -> x
 
-addAllBricks :: [(Coord3D, Coord3D)] -> Vector3D Int -> Vector3D Int
-addAllBricks bricks cube = foldl (\c (b, i) -> addBrick b i c) cube $ zip bricks [1..]
-
-addBrick :: (Coord3D, Coord3D) -> Int -> Vector3D Int -> Vector3D Int
-addBrick brick = fillCoords (brickCoords brick)
+addAllBricks :: [Brick] -> Vector3D Int -> Vector3D Int
+addAllBricks bricks cube = foldl (\c (b, i) -> fillCoords b i c) cube $ zip bricks [1..]
 
 fillCoords :: [Coord3D] -> Int -> Vector3D Int -> Vector3D Int
 fillCoords coords value cube = foldl (\acc c -> Vector3D.setAt c value acc) cube coords
 
-brickCoords :: (Coord3D, Coord3D) -> [Coord3D]
-brickCoords ((x,y,z), (x', y', z'))
+rawToSolidBrick :: RawBrick -> Brick
+rawToSolidBrick ((x,y,z), (x', y', z'))
   | x /= x' = [(x2,y,z) | x2 <- range x x']
   | y /= y' = [(x,y2,z) | y2 <- range y y']
   | z /= z' = [(x,y,z2) | z2 <- range z z']
