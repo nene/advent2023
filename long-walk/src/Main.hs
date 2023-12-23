b298d0d7549f930ff604e0e329d9124fa22a2818
Rene Saarsoo <nene@triin.net>
2023-12-23 09:55:31 +0200

    Day 23: Find start coordinate

--- long-walk/src/Main.hs
+++ long-walk/src/Main.hs
@@ -1,12 +1,18 @@
 module Main (main) where
 
-import Vector2D (Vector2D)
+import Vector2D (Vector2D, Coord2D)
 import qualified Vector2D
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ parseInput input
+  let hikeMap = parseInput input
+  print $ startCoord hikeMap
 
 parseInput :: String -> Vector2D Char
 parseInput = Vector2D.fromList . lines
+
+startCoord :: Vector2D Char -> Coord2D
+startCoord vector = fromMaybe (0,0) $ Vector2D.findCoord (== '.') vector
+
