df2466ba3a57cdda447ee1212152fb821fcc6623
Rene Saarsoo <nene@triin.net>
2023-12-23 10:45:23 +0200

    Day 23: possible directions

--- long-walk/src/Main.hs
+++ long-walk/src/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Vector2D (Vector2D, Coord2D)
+import Vector2D (Vector2D, Coord2D, at)
 import qualified Vector2D
 import Data.Maybe (fromMaybe)
 
@@ -8,7 +8,8 @@ main :: IO ()
 main = do
   input <- readFile "input.txt"
   let hikeMap = parseInput input
-  print $ startCoord hikeMap
+  let start = startCoord hikeMap
+  print $ possibleSteps start hikeMap
 
 parseInput :: String -> Vector2D Char
 parseInput = Vector2D.fromList . lines
@@ -16,3 +17,25 @@ parseInput = Vector2D.fromList . lines
 startCoord :: Vector2D Char -> Coord2D
 startCoord vector = fromMaybe (0,0) $ Vector2D.findCoord (== '.') vector
 
+-- walk :: Coord2D -> Vector2D Char -> Int
+-- walk startAt hikeMap =
+--   where
+--     stepsAt coord hmap = 
+
+
+possibleSteps :: Coord2D -> Vector2D Char -> [Coord2D]
+possibleSteps coord hmap = map snd $ filter (isValidStep hmap) $ possibleDirections coord
+
+isValidStep :: Vector2D Char -> (Dir, Coord2D) -> Bool
+isValidStep hmap (dir, coord) = case (hmap `at` coord, dir) of
+  (Just '.', _) -> True
+  (Just '>', R) -> True
+  (Just '<', L) -> True
+  (Just '^', U) -> True
+  (Just 'v', D) -> True
+  _ -> False
+
+data Dir = L | R | U | D deriving (Show, Eq)
+
+possibleDirections :: Coord2D-> [(Dir, Coord2D)]
+possibleDirections (x,y) = [(D, (x+1,y)), (U, (x-1,y)), (R, (x,y+1)), (L, (x,y-1))]
