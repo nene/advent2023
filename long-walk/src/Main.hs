e971bd1e912189b1ffebd3dc6445cb220f767236
Rene Saarsoo <nene@triin.net>
2023-12-23 09:46:41 +0200

    Day 23: Parse input

--- long-walk/src/Main.hs
+++ long-walk/src/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
+import Vector2D (Vector2D)
+import qualified Vector2D
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print input
+  print $ parseInput input
+
+parseInput :: String -> Vector2D Char
+parseInput = Vector2D.fromList . lines
