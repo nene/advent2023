555600750f527e2df4469fdca2d892937a94e6c8
Rene Saarsoo <nene@triin.net>
2023-12-23 11:05:14 +0200

    Day 23: Solve for example input

--- long-walk/src/Main.hs
+++ long-walk/src/Main.hs
@@ -3,25 +3,33 @@ module Main (main) where
 import Vector2D (Vector2D, Coord2D, at)
 import qualified Vector2D
 import Data.Maybe (fromMaybe)
+import Data.Foldable (maximumBy)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let hikeMap = parseInput input
   let start = startCoord hikeMap
-  print $ possibleSteps start hikeMap
+  let allWalks = walk start 0 hikeMap
+  let (maxLen, longestWalkMap) = maximumBy (\a b -> compare (fst a) (fst b)) allWalks
+  print maxLen
+  putStrLn $ showMap longestWalkMap
 
 parseInput :: String -> Vector2D Char
 parseInput = Vector2D.fromList . lines
 
+showMap :: Vector2D Char -> String
+showMap = unlines . Vector2D.toList
+
 startCoord :: Vector2D Char -> Coord2D
 startCoord vector = fromMaybe (0,0) $ Vector2D.findCoord (== '.') vector
 
--- walk :: Coord2D -> Vector2D Char -> Int
--- walk startAt hikeMap =
---   where
---     stepsAt coord hmap = 
-
+walk :: Coord2D -> Int -> Vector2D Char -> [(Int, Vector2D Char)]
+walk coord n hikeMap = case possibleSteps coord hikeMap' of
+    [] -> [(n, hikeMap')]
+    coords -> concatMap (\c -> walk c (n+1) hikeMap') coords
+  where
+    hikeMap' = Vector2D.setAt coord 'O' hikeMap
 
 possibleSteps :: Coord2D -> Vector2D Char -> [Coord2D]
 possibleSteps coord hmap = map snd $ filter (isValidStep hmap) $ possibleDirections coord
